26be0dcb982e5e187a99d2e993f97ddaa875d9a2
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-04-13 07:34:53 -0400

    Add player and script to control its movement

---
+++ SpaceAttackers/Assets/Project/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 1.5f;
+    public float horizontalLimit = 2.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(
+            Input.GetAxis("Horizontal") * speed,
+            0
+        );
+
+        if (transform.position.x > horizontalLimit)
+        {
+            transform.position = new Vector3(horizontalLimit, transform.position.y, transform.position.z);
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+
+        if (transform.position.x < -horizontalLimit)
+        {
+            transform.position = new Vector3(-horizontalLimit, transform.position.y, transform.position.z);
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+	}
+}
